dad4f6092738fae5d32304c40aed432b56c6b9fa
Shouhei Takai <shouheitakai.me@gmail.com>
2022-02-06 22:32:02 +0900

    実行エラーを修正、Noneタイプを.textで演算してしまうなど

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ for column in json_file_consolidated['columns']:
 df = pd.DataFrame(csv_column)
 
 # EDITNET APIからxbrlファイルだけを抽出してxbrlフォルダに入れる
-for i in range(1): # とりあえず3日分
-  day = date.today() + timedelta(days=i)
-  document_1day = document.get_documents("2022-01-11") #day
+for i in range(3): # とりあえず3日分
+  day = date.today() + timedelta(days=-i)
+  document_1day = document.get_documents(day)
 
   for i, document_item in enumerate(document_1day):
     if document_item["xbrlFlag"] == '1' and document_item["formCode"] == "043000" and document_item["docTypeCode"] == "140":
@@ -32,24 +32,28 @@ for i in range(1): # とりあえず3日分
       soup = file.read_xbrl(document_item["docID"])
       is_non_consolidated = bs.is_non_consolidated(soup)
       json_file = json_file_nonconsolidated if is_non_consolidated else json_file_consolidated
+
       for column in json_file['columns']:
         if column["label"] == "株価(終値)":
           # 株価取得
           data_close = data.get_dataframe_to_close(soup)
           csv_row.append(data_close)
+
         elif column["label"] == "時価総額":
           # 時価総額取得
           data_close = data.get_dataframe_to_close(soup)
           close_num = bs.get_item(soup, "jpcrp_cor:NumberOfIssuedSharesAsOfFiscalYearEndIssuedSharesTotalNumberOfSharesEtc", "FilingDateInstant")
-          csv_row.append(int(data_close) * int(close_num.text))
+          csv_row.append(int(0 if data_close == None else data_close) * int(0 if close_num == None else close_num.text))
+
         elif column["label"] == "連結または非連結":
           # 連結または非連結取得
           csv_row.append("非連結" if is_non_consolidated else "連結")
+
         else:
           # 行列に値を追加
           item = bs.get_item(soup, column["tag"], column["context"])
           csv_row.append("" if item == None else item.text)
-      
+      #
       df.loc[document_item["docID"]] = csv_row
       file.remove_trash(document_item["docID"])
 
--- utility/data.py
+++ utility/data.py
@@ -11,5 +11,9 @@ def get_dataframe_to_close(soup):
   current_period_item = bs.get_item(soup, "jpdei_cor:CurrentPeriodEndDateDEI", "FilingDateInstant")
   security_code_item = str(bs.get_item(soup, "jpdei_cor:SecurityCodeDEI", "FilingDateInstant").text).rstrip("0")
   current_period = str_to_date(current_period_item.text)
-  df_close = web.DataReader(f'{security_code_item}.T', 'yahoo', start=current_period, end=current_period)
-  return 0 if df_close.empty else int(df_close.at[current_period_item.text, "Close"])
+  df_close = None
+  try:
+    df_close = web.DataReader(f'{security_code_item}.T', 'yahoo', start=current_period, end=current_period)
+    0 if df_close.empty else int(df_close.at[current_period_item.text, "Close"])
+  except:
+    return 0
